c3dfc44e6c2395547495cb976645d7be35a3782d
edwin vakkachan <edwinvakkachan@gmail.com>
2026-05-03 00:16:34 +0530

    error:fixed

--- service/monitorService.js
+++ service/monitorService.js
@@ -2,45 +2,43 @@ import axios from "axios";
 import { getJellyfinSessions } from "./jellyfinService.js";
 import config from "../config/config.js";
 
+
 let lastActiveWatchTime = Date.now();
 
 export async function monitorJellyfinUsage() {
     console.log(`monitoring started`)
+
   const sessions = await getJellyfinSessions();
 
-
-
-for (const a of sessions) {
+  for (const a of sessions) {
   console.log(
     `User: ${a.UserName}\n` +
     `Playing: ${a.NowPlayingItem?.Name || "Nothing"}\n` +
     `Client: ${a.Client}\n`
   );
 }
-
-  const activeWatching = sessions.filter(
-    s => s.NowPlayingItem && !s.PlayState?.IsPaused
-  );
-
-  if (activeWatching.length > 0) {
+if(sessions.length==0){
+    console.log('No user is currently watching anything');
+}
+  // If sessions exist, users are watching
+  if (sessions.length > 0) {
     lastActiveWatchTime = Date.now();
     return;
   }
 
+  // Nobody watching
   const idleMinutes = (Date.now() - lastActiveWatchTime) / 1000 / 60;
 
+  console.log(`Idle for ${idleMinutes.toFixed(1)} minutes`);
+
   if (idleMinutes >= 30) {
-     try {
     await axios.post(config.WEBHOOK_JELLYFINOff, {
-      action: "turn_on"
+      action: "turn_off"
     });
 
-    res.send("✅ Device Turned ON");
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("❌ Failed to trigger");
-  }
-    console.log('device will turn off')
+    console.log("Device turned off");
+
+    // Reset timer to prevent repeated shutdown requests
     lastActiveWatchTime = Date.now();
   }
 }
