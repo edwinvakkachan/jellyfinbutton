815f22fc8b1f833d94e44a97ca4b7544f63ff435
edwin vakkachan <edwinvakkachan@gmail.com>
2026-05-05 22:05:40 +0530

    update:time changed

--- utils/logCurrentDateTime.js
+++ utils/logCurrentDateTime.js
@@ -1,18 +1,10 @@
-export async function logTime(message = "⌚") {
-  try {
-    const res = await fetch(
-      "https://worldtimeapi.org/api/timezone/Asia/Kolkata"
-    );
 
-    const data = await res.json();
 
-    const time = new Date(data.datetime).toLocaleString("en-IN", {
-      timeZone: "Asia/Kolkata",
-      hour12: false,
-    });
 
-    console.log(`${message} Monitoring started ${time}`);
-  } catch (err) {
-    console.error("Failed to fetch external time:", err);
-  }
+export async function logTime(message='⌚') {
+  const time = new Date().toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    hour12: false
+  });
+  console.log( `Monitoring started ${time} `);
 }
